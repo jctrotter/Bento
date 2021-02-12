5b7af4d669f6208c33d0d6dcb153d4f800bc1029
jctrotter <trotter.james.jct@gmail.com>
2021-02-11 17:35:45 -0800

    it me

--- js/greeting.js
+++ js/greeting.js
@@ -3,7 +3,7 @@ const today = new Date();
 const hour = today.getHours();
 
 // Here you can change your name
-const name = ' John Doe';
+const name = ' James';
 
 // Here you can change your greetings
 const gree1 = 'Sleepytime!  ';
